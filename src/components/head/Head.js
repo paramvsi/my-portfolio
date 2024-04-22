ea489a7b2091855b2e2a7377eafcc7175d540539
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-04-22 22:23:42 +0530

    Some Updates for app

--- src/components/head/Head.js
+++ src/components/head/Head.js
@@ -11,7 +11,15 @@ import {
 const Head = () => {
   return (
     <div className="py-3 lg:py-6 px-2 lg:px-3 bg-dark-teal text-white w-full flex justify-between align-middle">
-      <h2>Paramveer Singh</h2>
+      <h2 className="flex items-center justify-start gap-3">
+        {" "}
+        <img
+          className="w-6 h-6"
+          src="/assets/images/PSLogo.png"
+          alt="my-logo"
+        />{" "}
+        Paramveer Singh
+      </h2>
 
       <section className="flex justify-end gap-3">
         <span className="hidden lg:block">
--- src/components/main/projects/projects.js
+++ src/components/main/projects/projects.js
@@ -2,10 +2,17 @@ import React from "react";
 
 const Projects = () => {
   const projects = ["Touchnote.png", "BCBSMA.png", "Barclays.jpg", "ADIB.jpg"];
-  const personalProjects = ["ABC", "BDC"];
+  const personalProjects = [
+    {
+      name: "Stock Market Simulator",
+    },
+    {
+      name: "Food ordering app",
+    },
+  ];
 
   return (
-    <section className="py-4 px-6 lg:py-10 lg:px-24 bg-dark-teal lg:bg-teal-gradient text-center flex flex-col lg:flex-row justify-center items-center">
+    <section className="py-4 px-6 lg:py-10 lg:px-24 bg-dark-teal lg:bg-teal-gradient text-center flex flex-col lg:flex-row justify-center items-left">
       <div className="w-full lg:w-[50%] lg:border-r-2 lg:border-white">
         <h1 className="text-white text-2xl mb-10">Highlighted Projects</h1>
 
@@ -16,7 +23,7 @@ const Projects = () => {
               className="text-center cursor-pointer transform transition duration-300 hover:scale-105"
             >
               <img
-                className="w-16 h-16 mx-auto lg:w-28 lg:h-28 bg-white"
+                className="w-16 h-16 mx-auto lg:w-24 lg:h-24 xl:w-28 xl:h-28 bg-white"
                 src={`/assets/images/${project}`}
                 alt={project.split(".")[0]}
               />
@@ -27,26 +34,14 @@ const Projects = () => {
           ))}
         </div>
       </div>
-      <div className="w-full lg:w-[50%] my-5 lg:my-0">
+      <div className="w-full lg:w-[50%] mt-10 lg:my-0">
         <h1 className="text-white text-2xl mb-10 ">Personal Projects</h1>
 
-        <div className="flex justify-around items-center">
+        <ul className="text-left px-10">
           {personalProjects.map((project) => (
-            <div
-              key={project}
-              className="text-center cursor-pointer transform transition duration-300 hover:scale-105"
-            >
-              <img
-                className="w-16 h-16 mx-auto lg:w-28 lg:h-28 bg-white"
-                src={`/assets/images/${project}`}
-                alt={project.split(".")[0]}
-              />
-              <p className="text-white font-semibold mt-3">
-                {project.split(".")[0]}
-              </p>
-            </div>
+            <li className="text-white">{project.name}</li>
           ))}
-        </div>
+        </ul>
       </div>
     </section>
   );
